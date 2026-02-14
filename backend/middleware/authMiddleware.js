c890460870e081e69344fad33fcb23efd16a7291
cmiski <oshorwtt11@gmail.com>
2026-02-14 13:22:52 +0530

    added auth middleware

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,22 @@
+import { verifyToken } from "../utils/jwt.js";
+
+export default function authMiddleware(req, res, next) {
+  const header = req.headers.authorization;
+
+  if (!header) {
+    return res.status(401).json({ message: "No token provided" });
+  }
+
+  const token = header.split(" ")[1];
+  if (!token) {
+    return res.status(401).json({ message: "Invalid token format" });
+  }
+
+  try {
+    const decoded = verifyToken(token);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: "Invalid or expired token" });
+  }
+}
